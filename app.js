c72b2cdb15268260a1a5fd6ea7e0144e61de7fb2
Douglas Martins <douglas5422@outlook.com>
2022-02-26 16:57:05 -0300

    add

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const checkListRouter = require('./src/routes/checklist')
+
+const app = express();
+
+app.use('/checklists', checkListRouter);
+
+app.listen(3000, () => {
+    console.log('Servidor foi iniciado');
+})
